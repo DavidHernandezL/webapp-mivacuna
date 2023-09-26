3cd3794bde20e4b320c227eafe2f3633f0b54732
David Hernandez Lopez <HL_David@outlook.com>
2023-09-25 21:05:56 -0600

    Subida a la nube

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+//databases keys
+module.exports = {
+    database: {
+        host: 'bce7cf6g2paipzgdtdlz-mysql.services.clever-cloud.com',
+        user: 'uam4lhpsrrglbxiq',
+        password: 'G82kGjSAhrkGl65knDsT',
+        database: 'bce7cf6g2paipzgdtdlz'
+    }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const router = express.Router();
+const pool = require('../database');
+const passport = require('passport');
+var url, registro;
+
+//Index
+router.get('/', (req, res) => {
+    url = true;
+    registro = true;
+    profile = true;
+    res.render('partials/login.hbs', { url, registro, profile, });
+})
+
+router.post('/', passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+router.get('/registro', (req, res) => {
+    url = true;
+    registro = false;
+    profile = true;
+    res.render('partials/registro', { url, registro, profile });
+})
+
+router.get('/profile', (req, res) => {
+    profile = false;
+    url = false;
+    res.render('partials/profile', { url, profile });
+})
+
+router.post('/registro', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/registro',
+    failureFlash: true
+}));
+module.exports = router;
